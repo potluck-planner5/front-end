5ff5df787fed39a785d4db6274ef10b8113ecf15
Daniel Kish <aox.soljah@gmail.com>
2021-10-19 03:37:25 +0700

    fixed index.js routing, set local register state

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,8 +2,26 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import axios from "axios";
 
-const initialValue = {
+const initialValues = {
   email: "",
   username: "",
   password: "",
 };
+
+const Register = (props) => {
+  const { push } = useHistory();
+  const [credentials, setCredentials] = useState(initialValues);
+  const [error, setError] = useState();
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <h1>Register goes here</h1>
+    </div>
+  );
+};
+
+export default Register;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomePage from './Components/HomePage'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
-    <HomePage />
+    <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
