92b30936c142b250b3186b97eccd25ee55e476e8
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-18 22:07:10 -0500

    Finished layout of header, moving to linking

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from "react";
-import axios from "axios"
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-import * as yup from 'yup'
 
-
-import HomePage from './Components/HomePage'
-import Dashboard from './Components/Dashboard'
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
-        <Route>
-          {/* Sign up */}
-        </Route>
-        <Route>
-          {/* Login */}
-        </Route>
-        <Route path={'/dashboard'}>
-          <Dashboard />
-        </Route>
-      </Router>
-=======
 import logo from "./logo.svg";
 import "./App.css";
 import PrivateRoute from './Components/PrivateRoute';
 import axios from "axios";
 import HomePage from "./Components/HomePage";
-import { Route, Link, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import Register from './Components/Register';
 import LoginPage from "./Components/Login";
+import Dashboard from './Components/Dashboard'
 
 function App() {
   return (
@@ -48,8 +22,11 @@ function App() {
         <Route path={"/login"}>
           <LoginPage />
         </Route>
+        <Route path={'/dashboard'}>
+          <Dashboard />
+        </Route>
       </Switch>
->>>>>>> c140866182ec2047fd30a51a0e48907365e014dd
+
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,87 @@
 import React from 'react';
 
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { typography } from '@mui/system';
+import InputBase from '@mui/material/InputBase';
 
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: "10%",
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 15, 1, 2),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '0ch',
+        },
+      },
+    },
+  }));
 
 function Header() {
 
 
     return(
         <div>
-            <Box sx={{flexGrow: 1}}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="h2" component="div" sx={{flexGrow: 1}}>
-                            Potluck Planner
-                        </Typography>
-                        <Button color="inherit">Sign In</Button>
-                        <Button color="inherit">Register</Button>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            <Box sx={{flexGrow: 1}}>
-                <AppBar position="static">
+            <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h2" component="div" sx={{flexGrow: 1}}>
-                        Potluck Planner
-                    </Typography>
-                    <Button color="inherit">Sign In</Button>
-                    <Button color="inherit">Register</Button>
+                <Typography
+                variant="h3"
+                noWrap
+                marginRight="30%"
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                Potluck Planner
+                </Typography>
+                <Search>
+                    <SearchIconWrapper>
+                    <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                    <Button variant="contained">Sign In</Button>
+                    <Button variant="contained">Register</Button>
                 </Toolbar>
-                </AppBar>
+            </AppBar>
             </Box>
         </div>
      ) 
