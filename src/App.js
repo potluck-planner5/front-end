25287ae98e426f18fc2c399f1bfa0cfe0ff5dba1
Daniel Kish <aox.soljah@gmail.com>
2021-10-19 08:53:27 +0700

    conflicts resolved

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from "react";
-import axios from "axios"
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-import * as yup from 'yup'
-
-
-import HomePage from './Components/HomePage'
-import Dashboard from './Components/Dashboard'
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
-        <Route>
-          {/* Sign up */}
-        </Route>
-        <Route>
-          {/* Login */}
-        </Route>
-        <Route path={'/dashboard'}>
-          <Dashboard />
-        </Route>
-      </Router>
-=======
 import logo from "./logo.svg";
 import "./App.css";
-import PrivateRoute from './Components/PrivateRoute';
+import PrivateRoute from "./components/PrivateRoute";
 import axios from "axios";
-import HomePage from "./Components/HomePage";
+import HomePage from "./components/HomePage";
 import { Route, Link, Switch } from "react-router-dom";
-import Register from './Components/Register';
-import LoginPage from "./Components/Login";
+import Register from "./components/Register";
+import LoginPage from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -48,8 +21,10 @@ function App() {
         <Route path={"/login"}>
           <LoginPage />
         </Route>
+        <Route path={"/dashboard"}>
+          <Dashboard />
+        </Route>
       </Switch>
->>>>>>> c140866182ec2047fd30a51a0e48907365e014dd
     </div>
   );
 }
