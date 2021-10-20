ed3fbca7b6b4dfa22c47b9bc16a6fab2df5c81b1
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-20 10:55:34 -0500

    added mapping over mock guest list
    Maybe now?

--- src/Components/EventInfo.js
+++ src/Components/EventInfo.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Box from '@mui/material/Box'
 
 
+<<<<<<< HEAD
 
+=======
+const mockGuestList = [{name: "Bob", food: ["Milk", "Eggs", "Cheese","Napkins"]}, {name: "Sally", food: ["Salad", "Chips", "Salsa","Plates"]},{name: "George", food: ["Dessert", "Dip", "Drinks","Humor"]}]
+>>>>>>> d5d9c27 (added mapping over mock guest list)
 export default function EventInfo() {
     
     return (
@@ -13,6 +17,52 @@ export default function EventInfo() {
                     <p> Below are all guests that RSVP'd to your event, along with the items they are bringing</p>
                 </Box>
             </Box>
+<<<<<<< HEAD
+=======
+          
+            <Box backgroundColor="red">
+                <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', }}  width="70%"   ml="15%" backgroundColor="grey.300">
+                    {
+                        mockGuestList.map(guest => {
+                            return (
+                                <Box
+                                    sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    width:"30%",
+                                    ml: 2.5,
+                                    bgcolor: 'background.paper',
+                                    }}
+                                >
+                                    <Box sx={{ p: 1, bgcolor: 'grey.300' }}><h2>{guest.name}</h2></Box>
+                                    {
+                                        guest.food.map(item => {
+                                            return (
+                                                <Box sx={{ p: 1, bgcolor: 'grey.300' }}>{item}</Box>
+                                            )
+                                        })
+                                    }  
+                                </Box>
+                            )
+                        })
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                </Box>
+            </Box>
+>>>>>>> d5d9c27 (added mapping over mock guest list)
         </div>
     )
 }
