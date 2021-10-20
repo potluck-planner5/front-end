4e544e524baea1ef9dff0e10d0e02a3788a609f9
Christian <christiansmith2394@gmail.com>
2021-10-20 09:23:51 -0400

    Added some margin to the buttons on homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -45,7 +45,7 @@ export default function HomePage() {
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
           </p>
-          <Link to={"/login"} style={{ textDecoration: "none" }}>
+          <Link to={"/login"} style={{ textDecoration: "none", marginRight: '2%' }}>
             <Button variant="contained">Sign In</Button>
           </Link>
           <Link to={"/register"} style={{ textDecoration: "none" }}>
